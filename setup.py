5e74a0d46b247aff7d04d73d4b84f641bdc41c24
Aleš Kocjančič <alesdotio@gmail.com>
2013-09-06 17:54:50 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Ales Kocjancic",
     author_email="ales.kocjancic@divio.ch",
     name='django-shop-ajax',
-    version='0.1.2',
+    version='0.1.3',
     description='Ajax utilities for django SHOP',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='http://www.django-shop.org/',
